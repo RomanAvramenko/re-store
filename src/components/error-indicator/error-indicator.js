f16d9c3aa554927334c4b4a73a4497632e19ea65
unknown <ashkaell24@gmail.com>
2020-01-17 15:44:42 +0200

    first commit

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './error-indicator.css'
+
+const ErrorIndicator = () => {
+	return (
+		<div>
+			<h1>Something Went Wrong</h1>
+		</div>
+	)
+}
+
+export default ErrorIndicator
